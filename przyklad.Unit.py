a2483376519aa6431894d3b323ff7739163b4c81
holi87 <holi87@gmail.com>
2017-02-28 23:48:31 +0100

    test przechodzi bo funkcja jest juz poprawna

--- przyklad.Unit.py
+++ przyklad.Unit.py
@@ -10,5 +10,8 @@ class TestPrzyklad(unittest.TestCase):
     def test_czy_5_nie_jest_parzyste(self):
         self.assertFalse(przyklad.czy_parzysta(5))
 
+    def test_czy_1231_nie_jest_parzysta(self):
+        self.assertFalse(przyklad.czy_parzysta(1231))
+
 if __name__ == "__main__":
     unittest.main()
