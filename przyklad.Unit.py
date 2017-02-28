16f1f9164ffb0eb32e0752d36ae72f8444f72c9c
holi87 <holi87@gmail.com>
2017-02-28 23:38:37 +0100

    dokonczenie szkieletu unittestowego pliku

--- przyklad.Unit.py
+++ przyklad.Unit.py
@@ -2,3 +2,10 @@ import unittest2 as unittest
 __author__ = "Grzegorz Holak"
 
 class TestPrzyklad(unittest.TestCase):
+    def test(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
