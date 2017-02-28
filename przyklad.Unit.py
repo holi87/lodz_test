421f4e38a017d28cd65926101fc626119432ad34
holi87 <holi87@gmail.com>
2017-02-28 23:45:40 +0100

    dodajemy test czy 5 jest nie parzyste - blad

--- przyklad.Unit.py
+++ przyklad.Unit.py
@@ -7,6 +7,8 @@ class TestPrzyklad(unittest.TestCase):
     def test_czy_2_jest_parzyste(self):
         self.assertTrue(przyklad.czy_parzysta(2))
 
+    def test_czy_5_nie_jest_parzyste(self):
+        self.assertFalse(przyklad.czy_parzysta(5))
 
 if __name__ == "__main__":
     unittest.main()
