666adf75dfc8dd07a93167e7d147d85f1ee894d7
holi87 <holi87@gmail.com>
2017-02-28 23:41:05 +0100

    pierwszy test - czy dwa jest parzyste

--- przyklad.Unit.py
+++ przyklad.Unit.py
@@ -1,10 +1,10 @@
 import unittest2 as unittest
+import przyklad
 __author__ = "Grzegorz Holak"
 
 class TestPrzyklad(unittest.TestCase):
-    def test(self):
-        pass
-
+    def test_czy_2_jest_parzyste(self):
+        self.assertTrue(przyklad.czy_parzysta(2))
 
 
 if __name__ == "__main__":
